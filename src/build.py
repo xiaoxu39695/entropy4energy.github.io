7b242ab881dd3e7334b42c6fd1bd02ec3b191b6e
Marco Esters <marco.esters.phd@gmail.com>
2022-11-06 19:04:50 -0800

    Separate data processing and template filling

--- src/build.py
+++ src/build.py
@@ -17,6 +17,49 @@ DATA_DIR = os.path.join(BASE_PATH, "data")
 TEMPLATE_DIR = os.path.join(BASE_PATH, "templates")
 
 
+def process_publications(data):
+    pubs = {
+        "book": {
+        },
+        "journal": {
+        },
+    }
+    for typ in pubs.keys():
+        npubs = len(data["publications"][typ])
+        for p, pub in enumerate(data["publications"][typ]):
+            pub["number"] = npubs - p
+
+            if len(pub["authors"]) == 1:
+                authors = pub["author"][0]
+            elif len(pub["authors"]) == 2:
+                authors = " and ".join(pub["authors"])
+            else:
+                authors = ", ".join(pub["authors"][:-1]) + ", and " + pub["authors"][-1]
+            pub["authors"] = authors
+
+            if "journal" not in pub and "titleBook" not in pub:
+                pub["status"] = "submitted"
+            elif "doi" not in pub and "url" not in pub:
+                pub["status"] = "in press"
+
+            if "doi" in pub:
+                url = f'<a href="https://doi.org/{pub["doi"]} target="_blank">DOI:{pub["doi"]}</a>'
+            elif "arxiv" in pub:
+                url = f'<a href="https://arxiv.org/{pub["arxiv"]} target="_blank">ArXiV</a>'
+
+            year = pub["year"]
+            if year not in pubs[typ].keys():
+                pubs[typ][year] = [pub]
+            else:
+                pubs[typ][year].append(pub)
+    data["publications"] = pubs
+
+
+PROCESS_DATA = {
+    "publications": process_publications,
+}
+
+
 def arg_parser() -> argparse.ArgumentParser:
     """Parse command line arguments.
 
@@ -79,6 +122,8 @@ def build_html(section: str = "", extra_data: list = []) -> str:
             extra_file = f"{extra}.json"
         with open(os.path.join(DATA_DIR, extra_file)) as f:
             data[extra_section] = json.loads(f.read())
+    if section in PROCESS_DATA:
+        PROCESS_DATA[section](data)
 
     loader = FileSystemLoader(TEMPLATE_DIR)
     env = Environment(loader=loader)
