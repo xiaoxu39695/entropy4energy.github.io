15a992244a5b9b7987bab97416eae897cb3debd6
Marco Esters <marco.esters.phd@gmail.com>
2022-11-13 08:52:20 -0800

    Fix job status selector

--- src/js/jobs.js
+++ src/js/jobs.js
@@ -3,7 +3,7 @@
   for (const jobClosed of document.querySelectorAll('.job-date-close')) {
     const closeDate = new Date(jobClosed.innerText);
     if (closeDate < today) {
-      const jobStatus = jobClosed.nextElementSibling.querySelector('.job-date-stat');
+      const jobStatus = jobClosed.parentElement.nextElementSibling.querySelector('.job-date-stat');
       jobStatus.classList.remove("open");
       jobStatus.classList.add("closed");
       jobStatus.innerText = "Closed";
