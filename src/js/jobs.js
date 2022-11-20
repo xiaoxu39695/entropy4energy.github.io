97702784b26aa1ffb3402b2e623db1977d934da3
Marco Esters <marco.esters.phd@gmail.com>
2022-11-20 09:09:48 -0800

    Remove job status line if no javascript is enabled

--- src/js/jobs.js
+++ src/js/jobs.js
@@ -1,9 +1,10 @@
 (() => {
   const today = new Date();
   for (const jobClosed of document.querySelectorAll('.job-date-close')) {
-    const closeDate = new Date(jobClosed.innerText);
+    const closeDate = new Date(jobClosed.querySelector('.job-date-close-text').innerText);
     if (closeDate < today) {
-      const jobStatus = jobClosed.parentElement.nextElementSibling.querySelector('.job-date-stat');
+      jobClosed.nextElementSibling.classList.remove('noscript');  // see jobs.html template
+      const jobStatus = jobClosed.nextElementSibling.querySelector('.job-date-stat-text');
       jobStatus.classList.remove("open");
       jobStatus.classList.add("closed");
       jobStatus.innerText = "Closed";
