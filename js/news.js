e337fc581b139cbd29c6a74f2ae9f3a1d3d94d12
kclark202 <keith.t.clark00@gmail.com>
2023-02-09 17:32:50 -0500

    removed links to media on in js/news.js

--- js/news.js
+++ js/news.js
@@ -6,7 +6,7 @@
     +   '<span class="news-item-date">' + news.date + '</span>'
     +   '<span class="news-item-title">' + news.newsTitle + '</span>'
     +   '<span class="news-item-text">' + news.message + '</span>'
-    +   '<span class="news-item-img">' + (news.picture?('<a href="media/news/' + news.picture + '" target="blank"><img src="media/news/' + news.picture + '" alt="Image for news ' + news.newsTitle + '"></a>'):'') + '</span>'
+    +   '<span class="news-item-img">' + (news.picture?('<img src="media/news/' + news.picture + '" alt="Image for news ' + news.newsTitle + '">'):'') + '</span>'
     + '</div>';
     document.getElementById('news_items').insertAdjacentHTML('beforeend', newsHtml);
     newsId++;
